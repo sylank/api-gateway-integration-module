fd92d37d26ba5f1bf2d24a460df422fb555e9c57
sylank <serleg.dani@live.com>
2018-08-26 16:11:22 +0200

    locals

--- infrastructure/lavender-api-gateway.tf
+++ infrastructure/lavender-api-gateway.tf
@@ -1,8 +1,7 @@
 variable lambda_function_arn {}
 
 locals {
-  default_region = "${var.default_region}"
-  account_id = "${var.account_id}"
+  region = "${var.default_region}"
 }
 
 
@@ -30,7 +29,7 @@ resource "aws_api_gateway_integration" "integration" {
   http_method             = "${aws_api_gateway_method.method.http_method}"
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${local.default_region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
+  uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
 }
 
 output rest_api_id {
