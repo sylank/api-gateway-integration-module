6432f312b3c619c1c66ecc6a58d3c4cd2d8e724b
sylank <serleg.dani@live.com>
2018-08-26 14:32:57 +0200

    terraform script added

---
+++ infrastructure/lavender-api-gateway.tf
@@ -0,0 +1,41 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "api" {
+  name = "myapi"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part = "resource"
+  parent_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method.http_method}"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${data.aws_region.current}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
+}
+
+output rest_api_id {
+    value = "${aws_api_gateway_rest_api.api.id}"
+}
+
+output api_method {
+    value = "${aws_api_gateway_method.method.http_method}"
+}
+
+output api_resource {
+    value = "${aws_api_gateway_resource.resource.path}"
+}
