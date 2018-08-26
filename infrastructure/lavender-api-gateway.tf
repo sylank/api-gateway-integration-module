73ff544c4fd9a05f2e9d2647306ea5bceea32d50
sylank <serleg.dani@live.com>
2018-08-26 23:07:10 +0200

    small changes

--- infrastructure/lavender-api-gateway.tf
+++ infrastructure/lavender-api-gateway.tf
@@ -10,7 +10,7 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  path_part = "api"
+  path_part = "calendar"
   parent_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
 }
@@ -26,7 +26,7 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
   resource_id             = "${aws_api_gateway_resource.resource.id}"
   http_method             = "${aws_api_gateway_method.method.http_method}"
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
 }
@@ -35,7 +35,7 @@ resource "aws_api_gateway_deployment" "MyDemoDeployment" {
   depends_on = ["aws_api_gateway_integration.integration"]
 
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-  stage_name  = "prod"
+  stage_name  = "api"
 
   variables = {
     "answer" = "42"
