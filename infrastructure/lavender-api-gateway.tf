3323c4890aed22e622e22c1a0b586dee4128e7c1
sylank <serleg.dani@live.com>
2018-08-26 21:20:54 +0200

    gateway deployment

--- infrastructure/lavender-api-gateway.tf
+++ infrastructure/lavender-api-gateway.tf
@@ -26,7 +26,18 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
   resource_id             = "${aws_api_gateway_resource.resource.id}"
   http_method             = "${aws_api_gateway_method.method.http_method}"
-  integration_http_method = "POST"
+  integration_http_method = "GET"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
+}
+
+resource "aws_api_gateway_deployment" "MyDemoDeployment" {
+  depends_on = ["aws_api_gateway_integration.integration"]
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "prod"
+
+  variables = {
+    "answer" = "42"
+  }
 }
