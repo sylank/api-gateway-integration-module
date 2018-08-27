4a397aa43f0c50cfe22f52434110af4684e0063d
sylank <serleg.dani@live.com>
2018-08-27 22:56:44 +0200

    get

--- infrastructure/lavender-api-gateway.tf
+++ infrastructure/lavender-api-gateway.tf
@@ -26,7 +26,7 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
   resource_id             = "${aws_api_gateway_resource.resource.id}"
   http_method             = "${aws_api_gateway_method.method.http_method}"
-  integration_http_method = "POST"
+  integration_http_method = "GET"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
 }
