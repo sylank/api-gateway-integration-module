30a8e1499ab427ce1dd097da35228bfc1d6be9ab
sylank <serleg.dani@live.com>
2018-09-02 21:54:25 +0200

    lambda handler 2 added for test

--- infrastructure/lavender-api-gateway.tf
+++ infrastructure/lavender-api-gateway.tf
@@ -2,16 +2,15 @@ locals {
   region = "${data.aws_region.current.name}"
 }
 
-
 # API Gateway
 resource "aws_api_gateway_rest_api" "api" {
-  name = "lavender-backend-api"
+  name        = "lavender-backend-api"
   description = "Lavender backend services API gateway"
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  path_part = "calendar"
-  parent_id = "${aws_api_gateway_rest_api.api.root_resource_id}"
+  path_part   = "calendar"
+  parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
 }
 
@@ -29,6 +28,22 @@ resource "aws_api_gateway_integration" "integration" {
   integration_http_method = "GET"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}/invocations"
+}
+
+resource "aws_api_gateway_method" "method2" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration2" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.method2.http_method}"
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda2.arn}/invocations"
 }
 
 resource "aws_api_gateway_deployment" "MyDemoDeployment" {
