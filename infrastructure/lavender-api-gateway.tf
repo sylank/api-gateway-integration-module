babecfb794ecf01d735add813e0fe4fdcf2fbfa5
sylank <serleg.dani@live.com>
2018-09-14 22:23:42 +0200

    create reservation

--- infrastructure/lavender-api-gateway.tf
+++ infrastructure/lavender-api-gateway.tf
@@ -46,8 +46,27 @@ resource "aws_api_gateway_integration" "query_all_reservation_integration" {
   uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.query_all_reservation_lambda.arn}/invocations"
 }
 
+resource "aws_api_gateway_method" "create_reservation_method" {
+  rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
+  resource_id   = "${aws_api_gateway_resource.resource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "create_reservation_integration" {
+  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
+  resource_id             = "${aws_api_gateway_resource.resource.id}"
+  http_method             = "${aws_api_gateway_method.create_reservation_method.http_method}"
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.create_reservation_lambda.arn}/invocations"
+}
+
 resource "aws_api_gateway_deployment" "lavender_backend_deployment" {
-  depends_on = ["aws_api_gateway_integration.integration"]
+  depends_on = [
+                "aws_api_gateway_integration.reservation_enabled_integration",
+                "aws_api_gateway_integration.query_all_reservation_integration",
+                "aws_api_gateway_integration.create_reservation_integration"]
 
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "api"
