e9714281ce82509b0c7e5c7e2b7d603a6f654f59
sylank <serleg.dani@live.com>
2018-09-16 21:45:19 +0200

    api gateway deployment and lambda request processing lot of try and blood and sweat

--- infrastructure/lavender-api-gateway.tf
+++ infrastructure/lavender-api-gateway.tf
@@ -2,13 +2,57 @@ locals {
   region = "${data.aws_region.current.name}"
 }
 
+resource "random_string" "deployment_variable" {
+  length  = 10
+  special = false
+}
+
+resource "aws_api_gateway_account" "gateway" {
+  cloudwatch_role_arn = "${aws_iam_role.cloudwatchlog.arn}"
+}
+
+resource "aws_iam_role" "cloudwatchlog" {
+  name = "cloudwatchlog"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "cloudwatchlog" {
+  name       = "cloudwatchlog"
+  roles      = ["${aws_iam_role.cloudwatchlog.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+}
+
+resource "aws_api_gateway_method_settings" "settings" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  stage_name  = "${aws_api_gateway_deployment.lavender_backend_deployment.stage_name}"
+  method_path = "${aws_api_gateway_resource.reservation_root.path_part}/${aws_api_gateway_resource.reservation_enabled_resource.path_part}/${aws_api_gateway_method.reservation_enabled_method.http_method}"
+
+  settings {
+    metrics_enabled = true
+    logging_level   = "INFO"
+  }
+}
+
 # API Gateway
 resource "aws_api_gateway_rest_api" "api" {
   name        = "lavender-backend-api"
   description = "Lavender backend services API gateway"
 }
-
-
 
 resource "aws_api_gateway_resource" "reservation_root" {
   path_part   = "reservation"
@@ -56,14 +100,35 @@ resource "aws_api_gateway_method" "create_reservation_method" {
 }
 
 
+resource "aws_api_gateway_method_response" "200" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_resource.reservation_enabled_resource.id}"
+  http_method = "${aws_api_gateway_method.reservation_enabled_method.http_method}"
+  status_code = "200"
+  depends_on  = ["aws_api_gateway_integration.reservation_enabled_integration"]
+}
+
+resource "aws_api_gateway_integration_response" "default" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_resource.reservation_enabled_resource.id}"
+  http_method = "${aws_api_gateway_method.reservation_enabled_method.http_method}"
+  status_code       = "${aws_api_gateway_method_response.200.status_code}"
+  selection_pattern = ""
+
+  depends_on = ["aws_api_gateway_integration.reservation_enabled_integration"]
+}
 
 resource "aws_api_gateway_integration" "reservation_enabled_integration" {
   rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
   resource_id             = "${aws_api_gateway_resource.reservation_enabled_resource.id}"
   http_method             = "${aws_api_gateway_method.reservation_enabled_method.http_method}"
-  integration_http_method = "GET"
-  type                    = "AWS_PROXY"
+  integration_http_method = "POST"
+  type                    = "AWS"
   uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.reservation_enabled_lambda.arn}/invocations"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+  request_templates = {
+    "application/json" = "${file("api_gateway_body_mapping.template")}"
+  }
 }
 
 resource "aws_api_gateway_integration" "query_all_reservation_integration" {
@@ -84,18 +149,17 @@ resource "aws_api_gateway_integration" "create_reservation_integration" {
   uri                     = "arn:aws:apigateway:${local.region}:lambda:path/2015-03-31/functions/${aws_lambda_function.create_reservation_lambda.arn}/invocations"
 }
 
-
-
 resource "aws_api_gateway_deployment" "lavender_backend_deployment" {
   depends_on = [
-                "aws_api_gateway_integration.reservation_enabled_integration",
-                "aws_api_gateway_integration.query_all_reservation_integration",
-                "aws_api_gateway_integration.create_reservation_integration"]
+    "aws_api_gateway_integration.reservation_enabled_integration",
+    "aws_api_gateway_integration.query_all_reservation_integration",
+    "aws_api_gateway_integration.create_reservation_integration",
+  ]
 
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
   stage_name  = "api"
 
   variables = {
-    "answer" = "42"
+    "answer" = "${random_string.deployment_variable.result}"
   }
 }
