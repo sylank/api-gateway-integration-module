faffd07becaad09cd3256e1a982d5d7725a806be
sylank <serleg.dani@live.com>
2018-09-19 10:27:07 +0200

    separated into files

---
+++ api-gateway-lambda-module/response.tf
@@ -0,0 +1,18 @@
+
+resource "aws_api_gateway_method_response" "200" {
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  resource_id = "${aws_api_gateway_resource.resource.id}"
+  http_method = "${aws_api_gateway_method.method.http_method}"
+  status_code = "${var.status_200}"
+  depends_on  = ["aws_api_gateway_integration.integration"]
+}
+
+resource "aws_api_gateway_integration_response" "default_integration_response" {
+  rest_api_id       = "${aws_api_gateway_rest_api.api.id}"
+  resource_id       = "${aws_api_gateway_resource.resource.id}"
+  http_method       = "${aws_api_gateway_method.method.http_method}"
+  status_code       = "${var.status_default}"
+  selection_pattern = "${var.default_pattern}"
+
+  depends_on = ["aws_api_gateway_integration.integration"]
+}
